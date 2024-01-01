c03b7ccbf68c673575f3e019db0c0987118b96b6
dinethwishmitha <155346377+dinethwishmitha@users.noreply.github.com>
2024-01-01 13:55:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,14 +12,15 @@ global.location = 'Sri Lanka'
 global.gurl = 'https://instagram.com' // add your username
 global.sudo = process.env.SUDO || "94713466375"
 global.devs = '94713466375';
-global.website = 'https://github.com/Sithuwa/SITHUWA-MD' //wa.me/+94000000000000
+global.website = 'https://github.com/Sithuwa/SITHUWA-MD' //wa.me/+94713466375
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/15b1dd8aeaa47888d75d7.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "SITHUWA-MD;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0RYd05tRkt4dWRHL3VNU2hwRGFBYjhwMWV3Lzk5SlhYZ0praGNuVDJGUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQnRQU3RpRTlHQmMwaGxDMXFueEJFbDZMVEQvcy9SNnpSd2FLT0tMY1RYWT0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJrRmJyRS81MFpHWDNVckY2eFdrYUx1aFUwa1NhazVKTlRQb2c3UFRKSTAwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJOcHhLczhYZ1NhdjFVZUJwY2wxS3VwZ2RwbEp6THh2c09CV1YzMU11bVVnPSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IldELzJURHhodUNrTjVkSTZKT3N4elREczN4ckgvdWNqUysyS21RVUVpSEk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZNcVZYdUNVdTJ3MENOcjJoSi9UcTQvYjZhaVUyQ0ZXeWtBQ0kwVjUybUE9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsNEsrempiWTJkRW9iQkhKbmROaUFKK0c1Z21Ud2EwNW9Qa0syanUvS2dCQW9FbzBGVXJEOEloT3lkaFc4WWF0Tk1ucUZCSWliY1pRNkdaZ2xjYWtCUT09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjkzLCJhZHZTZWNyZXRLZXkiOiJ3RkpjcndNM0VYM3RuajI3ZGpLdWtvcEFUS1VJMm5tRU5RUTE4ZFY1bnJnPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJmZ2dCSUwwcVFwZXRHcXpxbkl1bzB3IiwicGhvbmVJZCI6IjYyY2RkZGNkLTI3OWQtNDRhZS1iODUwLWIxMmY1OTlhYTZkNCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIraWt6a2pzbmo5VEtzUUErTjVWNUFIaWZuUDA9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Iks0cTlNMktGc2xSb1NEaC9EbTlvZXJ5VnQwUT0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ05yK3Bwd0dFTVhCeWF3R0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6ImVDTlBFL2RYY25rNW1yemgzbDhWcHVtTU5FMmlsOTQyenl4dmhoY0hweVE9IiwiYWNjb3VudFNpZ25hdHVyZSI6IkRCVXEvU2c2enEzRUJwNzdLVGR3MmF0M2owQm9zc1ZTVWtvQWNwMW1kM2NiV1NpeWorai8xMVJOR2k1aTRvYXhpWFpqby84a3FXU3RJL0MvQWtsSER3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiI1MGs3TXNjUVY5OTNCZjJjdm1yWDk2TzlPUmJwdW1NNVFpT1NOM3M5c0M4LzJZWGxicmtBcnRLcllHVXh6SGlOVjR5ZGpaaWNpamthdWQ5a01qKy9Ddz09In0sIm1lIjp7ImlkIjoiOTQ3MTM0NjYzNzU6M0BzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLhn5rgv5Dig6LhtY/ig5jigbHigb/htY0g4LaH4oOi4La94LeK4LeG4LeP4oOi4ZKW4puT4oOi4Le04oOi4ZC14oOY4oOcIn0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6Ijk0NzEzNDY2Mzc1OjNAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWGdqVHhQM1YzSjVPWnE4NGQ1ZkZhYnBqRFJOb3BmZU5zOHNiNFlYQjZjayJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcwNDA5MTg1NH0=",      //Put Your Session Id Here
-  author:  process.env.PACK_AUTHER ||  'SITHUWA BOT',
-  packname:  process.env.PACK_NAME || 'MADE BY SITHUM KALHARA',
+  sessionName: process.env.SESSION_ID || "SITHUWA-MD;;;BenchActivist
+",      //Put Your Session Id Here
+  author:  process.env.PACK_AUTHER ||  'DINU BOY',
+  packname:  process.env.PACK_NAME || 'MADE BY MR ADHISHA',
   
-  botname:   process.env.BOT_NAME === undefined ? "SITHUWA-MD" : process.env.BOT_NAME,
+  botname:   process.env.BOT_NAME === undefined ? "KING ALFA-MD" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'dinu boy' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
@@ -27,7 +28,7 @@ module.exports = {
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
-  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
+  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? true : process.env.AUTO_STATUS_SAVER,
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
